952a8e5de9d2d08d8761b1ff3d3930a155fa229a
mrs_misteriya <nata.nedostupnaya@mail.com>
2024-03-31 23:32:17 +0300

    adds markup for 3 tasks

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import './App.module.css';
+import userData from '../Profile/userData.json';
+import friends from '../FriendList/friends.json';
+import transactions from '../TransactionHistory/transactions.json'
+import Profile from '../Profile/Profile';
+import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
+
+export default function App (){
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
